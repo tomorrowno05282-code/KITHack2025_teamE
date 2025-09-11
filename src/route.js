8c288f2228327f0650e72c194b99d27f0fac8811
Onn1223 <myharu8686@gmail.com>
2025-09-11 20:06:47 +0900

    検索カードからの経路検索。p

--- src/route.js
+++ src/route.js
@@ -10,28 +10,29 @@
      * 
      * 参考: https://www.liedman.net/leaflet-routing-machine/
      */
-    function planRoute(startPoint, endPoint){
-        // 既存の経路案内がある場合は削除
-        if (this.routingControl) {
-            this.map.removeControl(this.routingControl);
-        }
-
-        const start = places.find(place => place.names.includes(startPoint));
-        const end = places.find(place => place.names.includes(endPoint));
-        if (!start || !end) {
-            console.error("指定された開始点または終了点が見つかりません");
+    function planRoute(strlat,strlng, endlat, endlng){
+        clearRoute();
+        if (!strlat || !strlng || !endlat || !endlng) {
+            console.error("指定された座標が見つかりません");
             return;
         }
 
-        console.log(`経路案内を作成: ${startPoint} (${start.lat}, ${start.lng}) から ${endPoint} (${end.lat}, ${end.lng}) へ`);
+        console.log(`経路案内を作成: (${strlat}, ${strlng}) から (${endlat}, ${endlng}) へ`);
 
         this.routingControl = L.Routing.control({
             waypoints: [
-                L.latLng(start.lat, start.lng),
-                L.latLng(end.lat, end.lng)
+                L.latLng(strlat, strlng),
+                L.latLng(endlat, endlng)
             ],
             routeWhileDragging: true,
             autoRoute: true
         }).addTo(this.map);
 
+    }
+
+    function clearRoute(){
+        if (this.routingControl) {
+            this.map.removeControl(this.routingControl);
+            this.routingControl = null;
+        }
     }
