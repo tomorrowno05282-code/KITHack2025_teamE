538d06c7d81d7ed5b0e0f5658a8e4ef5b1aea606
MagicLor2205 <keiiiiiii765@gmail.com>
2025-09-11 17:10:57 +0900

    indexでusermarker.cssを読み込むように、マーカーを表示する処理のタイミングを現在地取得直後に変更

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,5 +1,6 @@
 let latitude;
 let longitude;
+let marker;
 
 const options = {
     enableHighAccuracy: true,
@@ -12,6 +13,7 @@ navigator.geolocation.watchPosition(success, error, options);
 function success(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
+    callBack();
 }
 
 function error(error) {
@@ -20,13 +22,18 @@ function error(error) {
     TIMEOUT = 3; // タイムアウト
     console.log(TIMEOUT);
 }
+    //setInterval(callBack, 5000); // ToDo:読み込み順を適切にする.
 
-setInterval(callBack, 5000); // ToDo:読み込み順を適切にする
 function callBack() {
-    // ToDo:リアルタイムで更新されるようにする
-    tileLayer.addTo(map);
+    // ToDo:リアルタイムで更新されるようにする.
     let options = {
         pulsing: true, accuracy: 1, smallIcon: true
     };
-    L.userMarker([latitude, longitude], options).addTo(map).bindPopup("pulsing true");
+    // L.userMarker([latitude, longitude], options).addTo(map).bindPopup("pulsing true");
+    if(marker == undefined){
+        marker = L.userMarker([latitude, longitude], options);
+        marker.addTo(map).bindPopup("現在地");
+    } else {
+        marker = L.userMarker([latitude, longitude], options);
+    }
 }
